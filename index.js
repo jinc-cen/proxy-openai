8075312838a50b90cd01fba98b4d6a941bf5ccb1
锦此 <a275727449@gmail.com>
2023-07-04 22:34:53 +0800

    fix :port

--- index.js
+++ index.js
@@ -38,4 +38,4 @@ app.post('/upload', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(process.env.PORT || 3000, () => console.log('Server running on port 3000'));
