1194ab7b28bce6c01ec502ed696e95cd89c48945
锦此 <a275727449@gmail.com>
2023-07-04 21:39:46 +0800

    feat: upload file

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-const http = require('http');
-const https = require('https');
+const express = require('express');
+const fs = require('fs');
+const { Configuration, OpenAIApi } = require('openai');
+const bodyParser = require('body-parser'); // To parse JSON bodies
 
-const proxy = (req, res) => {
-  // Ensure the request URL starts with "/v1/"
-  if (req.method === 'POST' && req.url.startsWith('/v1/')) {
-    let options = {
-      hostname: 'api.openai.com',
-      port: 443,
-      path: req.url,
-      method: req.method,
-      headers: req.headers
-    };
+const app = express();
+app.use(bodyParser.json());
 
-    const proxy = https.request(options, function (targetRes) {
-      res.writeHead(targetRes.statusCode, targetRes.headers);
-      targetRes.pipe(res, {
-        end: true
-      });
+app.post('/upload', async (req, res) => {
+  try {
+    const jsonData = req.body.data; // This is the JSON data we want to upload
+    const apiKey = req.headers['x-api-key']; // API key from the request headers
+
+    if (!apiKey) {
+      return res.status(401).send({ error: 'Missing API key' });
+    }
+
+    // Convert JSON to string and write to file
+    const filePath = 'mydata.jsonl';
+    fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+
+    // Create OpenAI API instance with the API key from the headers
+    const configuration = new Configuration({
+      apiKey: apiKey,
     });
+    const openai = new OpenAIApi(configuration);
 
-    req.pipe(proxy, {
-      end: true
-    });
-  } else {
-    res.writeHead(400);
-    res.end('Bad request: Only POST method is allowed, and URL must start with /v1/');
+    // Upload file to OpenAI
+    const response = await openai.createFile(
+      fs.createReadStream(filePath),
+      "application/json"
+    );
+
+    res.status(200).send(response);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: 'An error occurred' });
   }
-};
+});
 
-const server = http.createServer(proxy);
-server.listen(3000); // Assuming you want the proxy server to listen on port 3000
+app.listen(3000, () => console.log('Server running on port 3000'));
